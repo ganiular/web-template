5da6cee2cfd6e80c3bae868439756a5dabc76ddc
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-07-29 03:43:07 +0100

    refactor: Update LandingPage to use new HomePage component

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -10,7 +10,7 @@ import { propTypes } from '../../util/types';
 
 import FallbackPage from './FallbackPage';
 import { ASSET_NAME } from './LandingPage.duck';
-import MyLandingPage from '../../custom/container/LandingPage/MyLandingPage';
+import MyLandingPage from '../../custom/container/LandingPage/HomePage';
 
 const PageBuilder = loadable(() =>
   import(/* webpackChunkName: "PageBuilder" */ '../PageBuilder/PageBuilder')
@@ -18,9 +18,8 @@ const PageBuilder = loadable(() =>
 
 export const LandingPageComponent = props => {
   const { pageAssetsData, inProgress, error } = props;
-  console.log(pageAssetsData);
 
-  return <MyLandingPage />;
+  return <MyLandingPage />
 
   return (
     <PageBuilder
