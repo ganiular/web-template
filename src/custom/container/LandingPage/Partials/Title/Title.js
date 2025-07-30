5853208d14eed9ffb06dcfd26d67b8f096c17e54
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-07-30 22:27:50 +0100

    feat: Add FloatingIcon to Title component for enhanced visual appeal

--- src/custom/container/LandingPage/Partials/Title/Title.js
+++ src/custom/container/LandingPage/Partials/Title/Title.js
@@ -1,8 +1,10 @@
 import styles from "./Title.module.css";
+import FloatingIcon from "../Float/Float";
 
 export default function Title(props) {
     return (
         <h2  {...props} className={`${styles.root} ${props.className}`}>
+            {!props.hide && <FloatingIcon className={styles.floatingIcon} />}
             {props.children}
         </h2>
     );
