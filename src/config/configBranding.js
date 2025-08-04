e508d5a9eccbe442a3e8c9a8220b5b8b0a2c14f2
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-08-04 13:46:11 +0100

    refactor(branding): update desktop and mobile logos

--- src/config/configBranding.js
+++ src/config/configBranding.js
@@ -1,5 +1,5 @@
-import logoImageDesktop from '../assets/biketribe-logo-desktop.png';
-import logoImageMobile from '../assets/biketribe-logo-mobile.png';
+import logoImageDesktop from '../assets/logo.png';
+import logoImageMobile from '../assets/logo.png';
 import brandImage from '../assets/biketribe-brandImage-1500.jpg';
 import facebookImage from '../assets/biketribe-facebook-sharing-1200x630.jpg';
 import twitterImage from '../assets/biketribe-twitter-sharing-600x314.jpg';
